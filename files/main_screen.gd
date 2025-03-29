091bd4579a8cfec9d8400dd74f0c269eadf24e61
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-03-29 14:36:23 +0100

    added Dara

--- files/main_screen.gd
+++ files/main_screen.gd
@@ -2,6 +2,18 @@ extends Node2D
 
 func _ready():
 	pass
-	
+
 func _process(delta: float) -> void:
 	pass
+
+func _on_start_pressed():
+	Dialogic.start("timeline1")
+
+func _on_saves_pressed():
+	pass
+
+func _on_credits_pressed():
+	pass
+
+func _on_exit_pressed():
+	get_tree().quit()

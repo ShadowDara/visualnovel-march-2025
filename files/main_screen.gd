66424809185b75bf53d6d6d13acf84d748539832
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-03-30 14:57:36 +0200

    prepare for first version!

--- files/main_screen.gd
+++ files/main_screen.gd
@@ -1,11 +1,5 @@
 extends Node2D
 
-func _ready():
-	pass
-
-func _process(delta: float) -> void:
-	pass
-
 func _on_start_pressed():
 	Dialogic.start("timeline1")
 
@@ -13,7 +7,7 @@ func _on_saves_pressed():
 	pass
 
 func _on_credits_pressed():
-	pass
+	OS.shell_open("https://github.com/ShadowDara/visualnovel-march-2025")
 
 func _on_exit_pressed():
 	get_tree().quit()

14cca4dcfbbe4ecf4f64df80ec887d34620d2a8a
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-04-03 19:48:32 +0200

    Update main_screen.gd

--- files/main_screen.gd
+++ files/main_screen.gd
@@ -7,7 +7,7 @@ func _on_saves_pressed():
 	pass
 
 func _on_credits_pressed():
-	OS.shell_open("https://github.com/ShadowDara/visualnovel-march-2025")
+	OS.shell_open("https://github.com/weuritz8u/visualnovel-march-2025")
 
 func _on_exit_pressed():
 	get_tree().quit()
